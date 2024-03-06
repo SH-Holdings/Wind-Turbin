0f8ac7d038b4427e82f25aa66f2947cdc0447583
sotounsi <so.tounsi@aui.ma>
2024-03-06 23:37:28 +0100

    Add styled-components and update Hero and Navbar components

--- wind-turbin-app/src/components/Hero/Hero.js
+++ wind-turbin-app/src/components/Hero/Hero.js
@@ -1,21 +1,46 @@
 import React from "react";
-import "../../styles/Hero.css";
+import styled from "styled-components";
 
-function Hero() {
+// Assuming HeroSection is your component where the background image is set
+const HeroSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center; // Vertically centers content in the container
+  align-items: flex-start; // Aligns items to the start (left) of the container
+  height: 685px;
+  background-image: url("/images/mywindturbinehero.png");
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center right;
+  padding-left: 15%; // Adjust based on your design preference
+  color: #5392b0;
+`;
+
+const Title = styled.h1`
+  font-size: 3rem;
+  font-weight: 300;
+  margin: 0 0 20px 0;
+  // push to top
+  margin-top: -25%;
+  color: #5392b0;
+`;
+
+const Subtitle = styled.span`
+  font-size: 3rem; // Adjust size as needed
+  font-weight: 700;
+  display: block;
+  color: #333333;
+`;
+
+// Your React component
+const Hero = () => {
   return (
-    <div class="hero-section">
-      <div class="text-container">
-        <h1>
-          <span>Welcome to</span>
-          <span class="wind-turbine-title">
-            MY WIND
-            <br />
-            TURBINE
-          </span>
-        </h1>
-      </div>
-    </div>
+    <HeroSection>
+      <Title>
+        Welcome to <Subtitle>MY WIND TURBINE</Subtitle>
+      </Title>
+    </HeroSection>
   );
-}
+};
 
 export default Hero;
--- wind-turbin-app/src/components/NavBars/Navbar.js
+++ wind-turbin-app/src/components/NavBars/Navbar.js
@@ -1,29 +1,75 @@
-import React from "react";
-import "../../styles/Navbar.css";
+import React, { useState } from "react";
+import styled from "styled-components";
 
-// TODO: Add routers to the links
+const Nav = styled.nav`
+  background-color: #fff;
+  padding: 1rem 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const NavLinks = styled.div`
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    display: ${({ isOpen }) => (isOpen ? "flex" : "none")};
+  }
+`;
+
+const NavLink = styled.a`
+  text-decoration: none;
+  color: #333;
+  padding: 0.5rem 1rem;
+
+  &:hover {
+    color: #6200ea;
+  }
+`;
+
+const MobileIcon = styled.div`
+  display: none;
+
+  @media (max-width: 768px) {
+    display: flex;
+    font-size: 2rem;
+    cursor: pointer;
+  }
+`;
+const Logo = styled.img`
+  height: 75px;
+  cursor: pointer;
+  background-color: #ffffff;
+  @media (max-width: 768px) {
+    margin-bottom: 1rem;
+  }
+`;
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className="navbar">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a href="#">SLM Analysis</a>
-        </li>
-        <li className="nav-item">
-          <a href="#">Simulations</a>
-        </li>
-        <li className="nav-item">
-          <a href="#">About</a>
-        </li>
-        <li className="nav-item">
-          <a href="#">Contact</a>
-        </li>
-      </ul>
-    </nav>
+    <Nav>
+      {/* Reference the logo image using its path from the public directory */}
+      <Logo src="/images/logo.png" alt="Logo" />
+      <MobileIcon onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? "✖️" : "☰"}
+      </MobileIcon>
+      <NavLinks isOpen={isOpen}>
+        <NavLink href="#">Home</NavLink>
+        <NavLink href="#">SLM Analysis</NavLink>
+        <NavLink href="#">Simulations</NavLink>
+        <NavLink href="#">About</NavLink>
+        <NavLink href="#">Contact</NavLink>
+      </NavLinks>
+    </Nav>
   );
 }
 
